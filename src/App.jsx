13755c5e082d3c472a9e905732b8d312ad39380a
WaaDWaleD <waadwaled404@gmail.com>
2025-08-18 00:36:00 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Components/Home/Layout/Layout'
 import Home from './Components/Home/Home'
 import ItemDetails from './Components/ItemsDetails/ItemDetails'
 import Error from './Components/Home/Error/Error'
 
 function App() {
-  let router = createBrowserRouter([
+  let router = createHashRouter([
     {path:""  , element:<Layout></Layout> ,children:[
       {index:true , element: <Home></Home>},
       { path: "item/:id", element:<ItemDetails></ItemDetails> },
